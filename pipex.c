0dc791c4b915c4862715b15166faffc4919e2d88
Arzu GUNESLIK <agunesli@e1r6p16.42.fr>
2022-04-05 23:28:52 +0200

    check leak

--- pipex.c
+++ pipex.c
@@ -6,18 +6,20 @@
 /*   By: agunesli <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/20 16:07:27 by agunesli          #+#    #+#             */
-/*   Updated: 2022/03/30 00:38:22 by agunesli         ###   ########.fr       */
+/*   Updated: 2022/04/05 23:23:00 by agunesli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 #include <stdio.h>
 
-void	ft_child1(int *fd, int fdin, char *argv, char **env)
+void	ft_child1(int *fd, char *file, char *argv, char **env)
 {
 	char	**cmd_arg;
 	char	*path;
+	int		fdin;
 
+	fdin = open_file(file, 1);
 	if (close(fd[0]) == -1)
 		merror("Error with close\n");
 	if (dup2(fdin, STDIN_FILENO) == -1)
@@ -37,11 +39,13 @@ void	ft_child1(int *fd, int fdin, char *argv, char **env)
 	}
 }
 
-void	ft_child2(int *fd, int fdout, char *argv, char **env)
+void	ft_child2(int *fd, char *file, char *argv, char **env)
 {
 	char	**cmd_arg;
 	char	*path;
+	int		fdout;
 
+	fdout = open_file(file, 2);
 	if (close(fd[1]) == -1)
 		merror("Error with close\n");
 	if (dup2(fd[0], STDIN_FILENO) == -1)
@@ -69,13 +73,9 @@ int	main(int argc, char **argv, char **env)
 	int	child1;
 	int	child2;
 	int	fd[2];
-	int	fdin;
-	int	fdout;
 
 	if (argc != 5)
 		merror("nb d'arg no correct\n");
-	fdin = open_file(argv[1], 1);
-	fdout = open_file(argv[4], 2);
 	if (pipe(fd) == -1)
 		merror("Error with pipe\n");
 	child1 = fork();
@@ -83,9 +83,9 @@ int	main(int argc, char **argv, char **env)
 	if (child1 == -1 || child2 == -1)
 		merror("Error with fork child\n");
 	if (child1 == 0 && child2 != 0)
-		ft_child1(fd, fdin, argv[2], env);
+		ft_child1(fd, argv[1], argv[2], env);
 	if (child2 == 0 && child1 != 0)
-		ft_child2(fd, fdout, argv[3], env);
+		ft_child2(fd, argv[4], argv[3], env);
 	if (close(fd[1]) == -1 || close(fd[0]) == -1)
 		merror("Error with close\n");
 	waitpid(child1, NULL, 0);
